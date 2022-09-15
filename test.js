9fb9e5dab01f34fa1375387e940020d1243b84e4
ifeanyihm <iheme.ifeanyi@yahoo.com>
2022-09-15 09:01:27 +0100

    My first project

---
+++ test.js
@@ -0,0 +1,152 @@
+
+function closeMe() {
+    x=document.getElementById("one");
+    x.style.display='block';
+
+    x=document.getElementById("two");
+    x.style.display='block';
+
+    x=document.getElementById("three");
+    x.style.display='block';
+
+    x=document.getElementById("four");
+    x.style.display='block';
+
+    x=document.getElementById("five");
+    x.style.display='block';
+}
+
+function clickMe() {
+    x=document.getElementById("one");
+    x.style.display='none';
+
+    x=document.getElementById("two");
+    x.style.display='block';
+
+    x=document.getElementById("three");
+    x.style.display='none';
+
+    x=document.getElementById("four");
+    x.style.display='none';
+
+    x=document.getElementById("five");
+    x.style.display='none';
+}
+
+function clothMe() {
+    x=document.getElementById("one");
+    x.style.display='none';
+
+    x=document.getElementById("two");
+    x.style.display='none';
+
+    x=document.getElementById("three");
+    x.style.display='block';
+
+    x=document.getElementById("four");
+    x.style.display='none';
+
+    x=document.getElementById("five");
+    x.style.display='block';
+}
+
+function cloudMe() {
+    x=document.getElementById("one");
+    x.style.display='none';
+
+    x=document.getElementById("two");
+    x.style.display='none';
+
+    x=document.getElementById("three");
+    x.style.display='none';
+
+    x=document.getElementById("four");
+    x.style.display='block';
+
+    x=document.getElementById("five");
+    x.style.display='none';
+}
+
+function claimMe() {
+    x=document.getElementById("one");
+    x.style.display='none';
+
+    x=document.getElementById("two");
+    x.style.display='none';
+
+    x=document.getElementById("three");
+    x.style.display='block';
+
+    x=document.getElementById("four");
+    x.style.display='none';
+
+    x=document.getElementById("five");
+    x.style.display='block';
+}
+
+function climbMe() {
+    x=document.getElementById("one");
+    x.style.display='block';
+
+    x=document.getElementById("two");
+    x.style.display='none';
+
+    x=document.getElementById("three");
+    x.style.display='block';
+
+    x=document.getElementById("four");
+    x.style.display='none';
+
+    x=document.getElementById("five");
+    x.style.display='block';
+}
+function countMe() {
+    x=document.getElementById("one");
+    x.style.display='none';
+
+    x=document.getElementById("two");
+    x.style.display='none';
+
+    x=document.getElementById("three");
+    x.style.display='none';
+
+    x=document.getElementById("four");
+    x.style.display='none';
+
+    x=document.getElementById("five");
+    x.style.display='block';
+}
+
+function crowdMe() {
+    x=document.getElementById("one");
+    x.style.display='none';
+
+    x=document.getElementById("two");
+    x.style.display='block';
+
+    x=document.getElementById("three");
+    x.style.display='block';
+
+    x=document.getElementById("four");
+    x.style.display='block';
+
+    x=document.getElementById("five");
+    x.style.display='block';
+}
+
+function craneMe() {
+    x=document.getElementById("one");
+    x.style.display='none';
+
+    x=document.getElementById("two");
+    x.style.display='none';
+
+    x=document.getElementById("three");
+    x.style.display='block';
+
+    x=document.getElementById("four");
+    x.style.display='block';
+
+    x=document.getElementById("five");
+    x.style.display='block';
+}
